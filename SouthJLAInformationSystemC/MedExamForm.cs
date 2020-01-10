c43d45acdc1560aaddf724b0b1e073e86070c2e1
John Paul Reyes <jp19reyes@gmail.com>
2020-01-10 10:05:02 +0800

    35 lines

--- SouthJLAInformationSystemC/MedExamForm.cs
+++ SouthJLAInformationSystemC/MedExamForm.cs
@@ -14,9 +14,8 @@ namespace SouthJLAInformationSystemC
     public partial class MedExamForm : Form
     {
         public string passID, type, gender, civilStat;
-        public string med1,med2, holder;
         public int med;
-       
+
 
         private void SubmitCBC_Click(object sender, EventArgs e)
         {
@@ -24,92 +23,33 @@ namespace SouthJLAInformationSystemC
 
             if (type == "Enter")
             {
-   //             sqlString = "INSERT INTO dbo.Hematology (wbc, rbc, hgb, hct, platelets, neutrophil, lymphocytes, monocyte, ofw_id) VALUES('" + wbcTextBox.Text + "','" + rbcTextBox.Text + "','" + hgbTextBox.Text + "','" + hctTextBox.Text + "','" + plateletsTextBox.Text + "','" + neutrophilTextBox.Text + "','" + lymphocytesTextBox.Text + "','" + monocyteTextBox.Text + "','" + passID + "')";
-   //             string[] valueString = { wbcTextBox.Text, rbcTextBox.Text, hgbTextBox.Text, hctTextBox.Text, plateletsTextBox.Text, neutrophilTextBox.Text, lymphocytesTextBox.Text, monocyteTextBox.Text };
-    //            string[] patientInfoValue = { idBox.Text, lastBox.Text, firstBox.Text, middleBox.Text, ageBox.Text, gender, civilStat };
-    //            VerifyPopUp verifyPopUp = new VerifyPopUp(sqlString, valueString, patientInfoValue);
-     //           verifyPopUp.Show();
+                //             sqlString = "INSERT INTO dbo.Hematology (wbc, rbc, hgb, hct, platelets, neutrophil, lymphocytes, monocyte, ofw_id) VALUES('" + wbcTextBox.Text + "','" + rbcTextBox.Text + "','" + hgbTextBox.Text + "','" + hctTextBox.Text + "','" + plateletsTextBox.Text + "','" + neutrophilTextBox.Text + "','" + lymphocytesTextBox.Text + "','" + monocyteTextBox.Text + "','" + passID + "')";
+                //             string[] valueString = { wbcTextBox.Text, rbcTextBox.Text, hgbTextBox.Text, hctTextBox.Text, plateletsTextBox.Text, neutrophilTextBox.Text, lymphocytesTextBox.Text, monocyteTextBox.Text };
+                //            string[] patientInfoValue = { idBox.Text, lastBox.Text, firstBox.Text, middleBox.Text, ageBox.Text, gender, civilStat };
+                //            VerifyPopUp verifyPopUp = new VerifyPopUp(sqlString, valueString, patientInfoValue);
+                //           verifyPopUp.Show();
             }
             else if (type == "Save changes")
             {
-     //           sqlString = "UPDATE dbo.Hematology SET wbc = '" + wbcTextBox.Text + "', rbc = '" + rbcTextBox.Text + "', hgb = '" + hgbTextBox.Text + "', hct = '" + hctTextBox.Text + "', platelets = '" + plateletsTextBox.Text + "', neutrophil = '" + neutrophilTextBox.Text + "', lymphocytes = '" + lymphocytesTextBox.Text + "', monocyte = '" + monocyteTextBox.Text + "' WHERE ofw_id = '" + passID + "'";
-     //           string[] valueString = { wbcTextBox.Text, rbcTextBox.Text, hgbTextBox.Text, hctTextBox.Text, plateletsTextBox.Text, neutrophilTextBox.Text, lymphocytesTextBox.Text, monocyteTextBox.Text };
-     //           string[] patientInfoValue = { idBox.Text, lastBox.Text, firstBox.Text, middleBox.Text, ageBox.Text, gender, civilStat };
-     //           VerifyPopUp verifyPopUp = new VerifyPopUp(sqlString, valueString, patientInfoValue);
-    //            verifyPopUp.Show();
+                //           sqlString = "UPDATE dbo.Hematology SET wbc = '" + wbcTextBox.Text + "', rbc = '" + rbcTextBox.Text + "', hgb = '" + hgbTextBox.Text + "', hct = '" + hctTextBox.Text + "', platelets = '" + plateletsTextBox.Text + "', neutrophil = '" + neutrophilTextBox.Text + "', lymphocytes = '" + lymphocytesTextBox.Text + "', monocyte = '" + monocyteTextBox.Text + "' WHERE ofw_id = '" + passID + "'";
+                //           string[] valueString = { wbcTextBox.Text, rbcTextBox.Text, hgbTextBox.Text, hctTextBox.Text, plateletsTextBox.Text, neutrophilTextBox.Text, lymphocytesTextBox.Text, monocyteTextBox.Text };
+                //           string[] patientInfoValue = { idBox.Text, lastBox.Text, firstBox.Text, middleBox.Text, ageBox.Text, gender, civilStat };
+                //           VerifyPopUp verifyPopUp = new VerifyPopUp(sqlString, valueString, patientInfoValue);
+                //            verifyPopUp.Show();
             }
         }
 
-        private static bool ComputeCondition(string value)
-        {
-            using (DataTable dt = new DataTable())
-            {
-                return(bool)(dt.Compute(value, null));
-            }
-        }
-       
-        private void encodeBinary()
-        {
-        
-            for (int i=1;i<36;i++)
-            {
-                holder = $"y{i.ToString()}.Checked == true";
-                if (ComputeCondition(holder))
-                {
-                    med += 10 ^ (i-1);
-                }
-            }
 
-            /*        if (y1.Checked == true) med += 1;
-             if (y2.Checked == true) med += 1;
- if (y3.Checked == true) med += 10;
- if (y4.Checked == true) med += 100;
- if (y5.Checked == true) med += 1000;
- if (y6.Checked == true) med += 10000;
- if (y7.Checked == true) med += 100000;
-  if (y8.Checked == true) med += 1;
- if (y9.Checked == true) med += 1;
- if (y10.Checked == true) med += 1;
- if (y11.Checked == true) med += 1;
- if (y12.Checked == true) med += 1;
- if (y13.Checked == true) med += 1;
- if (y14.Checked == true) med += 1;
- if (y15.Checked == true) med += 1;
- if (y16.Checked == true) med += 1;
- if (y17.Checked == true) med += 1;
-if (y18.Checked == true) med += 1;
- if (y19.Checked == true) med += 1;
- if (y20.Checked == true) med += 1;
- if (y21.Checked == true) med += 1;
- if (y22.Checked == true) med += 1;
-  if (y23.Checked == true) med += 1;
- if (y24.Checked == true) med += 1;
- if (y25.Checked == true) med += 1;
- if (y26.Checked == true) med += 1;
-  if (y27.Checked == true) med += 1;
- if (y28.Checked == true) med += 1;
- if (y29.Checked == true) med += 1;
- if (y30.Checked == true) med += 1;
- if (y31.Checked == true) med += 1;
- if (y32.Checked == true) med += 1;
- if (y33.Checked == true) med += 1;
- if (y34.Checked == true) med += 1;
- if (y35.Checked == true) med += 1;
 
-            */
-        }
 
-        private void decryptBinary()
-        {
 
-        }
 
         public MedExamForm(string uniqueID, string idPass, string type)
         {
             InitializeComponent();
 
             this.type = type;
-            
+
             childBox.Text = "";
             pastBox.Text = "";
             presentBox1.Text = "";
@@ -185,8 +125,8 @@ if (y18.Checked == true) med += 1;
                 DataTable dt2 = new DataTable(); //this is creating a virtual table  
                 sdaFill.Fill(dt2);
 
-      //          med1 = dt2.Rows[0][1].ToString;
-       //         med2 = dt2.Rows[0][2].ToString;
+                //          med1 = dt2.Rows[0][1].ToString;
+                //         med2 = dt2.Rows[0][2].ToString;
                 impressionBox.Text = dt2.Rows[0][3].ToString();
                 recommendationBox.Text = dt2.Rows[0][4].ToString();
                 heightBox.Text = dt2.Rows[0][5].ToString();
@@ -246,6 +186,119 @@ if (y18.Checked == true) med += 1;
 
         }
 
+        private void decryptBinary()
+        {
+            int[] medArray = new int[35];
+            int counter = 0;
+            for (counter = 35; counter > 0; counter--)
+            {
+                if (med >= Math.Pow(10, counter - 1))
+                    medArray[counter - 1] = 1;
+            }
 
-    }
+            for (int counter2 = 0; counter < 35; counter++)
+            {
+                if (medArray[counter2] == 1){y1.Checked = true;n1.Checked = false;}else{n1.Checked = true;y1.Checked = false;}
+                if (medArray[counter2] == 1) { y2.Checked = true; n2.Checked = false; } else { n1.Checked = true; y1.Checked = false; }
+                if (medArray[counter2] == 1) { y3.Checked = true; n3.Checked = false; } else { n2.Checked = true; y2.Checked = false; }
+                if (medArray[counter2] == 1) { y4.Checked = true; n4.Checked = false; } else { n3.Checked = true; y3.Checked = false; }
+                if (medArray[counter2] == 1) { y5.Checked = true; n5.Checked = false; } else { n4.Checked = true; y4.Checked = false; }
+                if (medArray[counter2] == 1) { y6.Checked = true; n6.Checked = false; } else { n5.Checked = true; y5.Checked = false; }
+                if (medArray[counter2] == 1) { y7.Checked = true; n7.Checked = false; } else { n6.Checked = true; y6.Checked = false; }
+                if (medArray[counter2] == 1) { y8.Checked = true; n8.Checked = false; } else { n7.Checked = true; y7.Checked = false; }
+                if (medArray[counter2] == 1) { y9.Checked = true; n9.Checked = false; } else { n8.Checked = true; y8.Checked = false; }
+                if (medArray[counter2] == 1) { y10.Checked = true; n10.Checked = false; } else { n9.Checked = true; y9.Checked = false; }
+                if (medArray[counter2] == 1) { y11.Checked = true; n11.Checked = false; } else { n10.Checked = true; y10.Checked = false; }
+                if (medArray[counter2] == 1) { y12.Checked = true; n12.Checked = false; } else { n11.Checked = true; y11.Checked = false; }
+                if (medArray[counter2] == 1) { y13.Checked = true; n13.Checked = false; } else { n12.Checked = true; y12.Checked = false; }
+                if (medArray[counter2] == 1) { y14.Checked = true; n14.Checked = false; } else { n13.Checked = true; y13.Checked = false; }
+                if (medArray[counter2] == 1) { y15.Checked = true; n15.Checked = false; } else { n14.Checked = true; y14.Checked = false; }
+                if (medArray[counter2] == 1) { y16.Checked = true; n16.Checked = false; } else { n15.Checked = true; y15.Checked = false; }
+                if (medArray[counter2] == 1) { y17.Checked = true; n17.Checked = false; } else { n16.Checked = true; y16.Checked = false; }
+                if (medArray[counter2] == 1) { y18.Checked = true; n18.Checked = false; } else { n17.Checked = true; y17.Checked = false; }
+                if (medArray[counter2] == 1) { y19.Checked = true; n19.Checked = false; } else { n18.Checked = true; y18.Checked = false; }
+                if (medArray[counter2] == 1) { y20.Checked = true; n20.Checked = false; } else { n19.Checked = true; y19.Checked = false; }
+                if (medArray[counter2] == 1) { y21.Checked = true; n21.Checked = false; } else { n20.Checked = true; y20.Checked = false; }
+                if (medArray[counter2] == 1) { y22.Checked = true; n22.Checked = false; } else { n21.Checked = true; y21.Checked = false; }
+                if (medArray[counter2] == 1) { y23.Checked = true; n23.Checked = false; } else { n22.Checked = true; y22.Checked = false; }
+                if (medArray[counter2] == 1) { y24.Checked = true; n24.Checked = false; } else { n23.Checked = true; y23.Checked = false; }
+                if (medArray[counter2] == 1) { y25.Checked = true; n25.Checked = false; } else { n24.Checked = true; y24.Checked = false; }
+                if (medArray[counter2] == 1) { y26.Checked = true; n26.Checked = false; } else { n25.Checked = true; y25.Checked = false; }
+                if (medArray[counter2] == 1) { y27.Checked = true; n27.Checked = false; } else { n26.Checked = true; y26.Checked = false; }
+                if (medArray[counter2] == 1) { y28.Checked = true; n28.Checked = false; } else { n27.Checked = true; y27.Checked = false; }
+                if (medArray[counter2] == 1) { y29.Checked = true; n29.Checked = false; } else { n28.Checked = true; y28.Checked = false; }
+                if (medArray[counter2] == 1) { y30.Checked = true; n30.Checked = false; } else { n29.Checked = true; y29.Checked = false; }
+                if (medArray[counter2] == 1) { y31.Checked = true; n31.Checked = false; } else { n30.Checked = true; y30.Checked = false; }
+                if (medArray[counter2] == 1) { y32.Checked = true; n32.Checked = false; } else { n31.Checked = true; y31.Checked = false; }
+                if (medArray[counter2] == 1) { y33.Checked = true; n33.Checked = false; } else { n32.Checked = true; y32.Checked = false; }
+                if (medArray[counter2] == 1) { y34.Checked = true; n34.Checked = false; } else { n33.Checked = true; y33.Checked = false; }
+                if (medArray[counter2] == 1) { y35.Checked = true; n35.Checked = false; } else { n34.Checked = true; y34.Checked = false; }
+
+            }
+        }
+     
+
+    private void encodeBinary()
+        {
+            /*
+                for (int i=1;i<36;i++)
+                {
+                    holder = $"y{i.ToString()}.Checked == true";
+                    if (ComputeCondition(holder))
+                    {
+                        med += 10 ^ (i-1);
+                    }
+                }
+             To the future programmer: This is the logic for this function, all that is needed is the
+             correct syntax and you can delete whats below
+                */
+
+            if (y1.Checked == true) med += 1;
+            if (y2.Checked == true) med += 10;
+            if (y3.Checked == true) med += 10 ^ 2;
+            if (y4.Checked == true) med += 10 ^ 3;
+            if (y5.Checked == true) med += 10 ^ 4;
+            if (y6.Checked == true) med += 10 ^ 5;
+            if (y7.Checked == true) med += 10 ^ 6;
+            if (y8.Checked == true) med += 10 ^ 7;
+            if (y9.Checked == true) med += 10 ^ 8;
+            if (y10.Checked == true) med += 10 ^ 9;
+            if (y11.Checked == true) med += 10 ^ 10;
+            if (y12.Checked == true) med += 10 ^ 11;
+            if (y13.Checked == true) med += 10 ^ 12;
+            if (y14.Checked == true) med += 10 ^ 13;
+            if (y15.Checked == true) med += 10 ^ 14;
+            if (y16.Checked == true) med += 10 ^ 15;
+            if (y17.Checked == true) med += 10 ^ 16;
+            if (y18.Checked == true) med += 10 ^ 17;
+            if (y19.Checked == true) med += 10 ^ 18;
+            if (y20.Checked == true) med += 10 ^ 19;
+            if (y21.Checked == true) med += 10 ^ 20;
+            if (y22.Checked == true) med += 10 ^ 21;
+            if (y23.Checked == true) med += 10 ^ 22;
+            if (y24.Checked == true) med += 10 ^ 23;
+            if (y25.Checked == true) med += 10 ^ 24;
+            if (y26.Checked == true) med += 10 ^ 25;
+            if (y27.Checked == true) med += 10 ^ 26;
+            if (y28.Checked == true) med += 10 ^ 27;
+            if (y29.Checked == true) med += 10 ^ 28;
+            if (y30.Checked == true) med += 10 ^ 29;
+            if (y31.Checked == true) med += 10 ^ 30;
+            if (y32.Checked == true) med += 10 ^ 31;
+            if (y33.Checked == true) med += 10 ^ 32;
+            if (y34.Checked == true) med += 10 ^ 33;
+            if (y35.Checked == true) med += 10 ^ 34;
+
+        }
+        /*  this is part of the encode binary without the correct syntax
+         *  private static bool ComputeCondition(string value)
+          {
+              using (DataTable dt = new DataTable())
+              {
+                  return (bool)(dt.Compute(value, null));
+              }
+          }
+          */
+
+    } 
 }
