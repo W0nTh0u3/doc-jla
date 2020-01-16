820f95518af2ec8970163e93ea4f67f5e013500e
Pong <jp19reyes@gmail.com>
2020-01-16 11:20:31 +0800

    offsite search

--- SouthJLAInformationSystemC/OffsiteEntEdtForm.cs
+++ SouthJLAInformationSystemC/OffsiteEntEdtForm.cs
@@ -37,23 +37,22 @@ namespace SouthJLAInformationSystemC
                 SqlDataAdapter sdaSearch = new SqlDataAdapter("SELECT * FROM dbo.mjrl2020 WHERE patientID = '" + searchBox.Text + "'", conn);
                 DataTable dt = new DataTable(); //this is creating a virtual table  
                 sdaSearch.Fill(dt);
-
-                DateTime date1 = Convert.ToDateTime(dt.Rows[0][5].ToString());
+                //    DateTime date1 = Convert.ToDateTime(dt.Rows[0][14].ToString()); Not needed yet
 
                 lastBox.Text = dt.Rows[0][1].ToString();
                 firstBox.Text = dt.Rows[0][2].ToString();
                 middleBox.Text = dt.Rows[0][3].ToString();
                 ageBox.Text = dt.Rows[0][4].ToString();
-                addressBox.Text = dt.Rows[0][8].ToString();
-                genderBox.SelectedItem = dt.Rows[0][6].ToString();
-                civilBox.SelectedItem = dt.Rows[0][7].ToString();
-                uniquePass = dt.Rows[0][11].ToString();
+                addressBox.Text = dt.Rows[0][5].ToString();
+                genderBox.SelectedItem = dt.Rows[0][7].ToString();
+                civilBox.SelectedItem = dt.Rows[0][6].ToString();
+                uniquePass = dt.Rows[0][15].ToString();
                 idPass = dt.Rows[0][0].ToString();
-                bdayBox.Value = date1;
-                packageBox.SelectedItem = dt.Rows[0][10].ToString();
-                paymentStatusBox.SelectedItem = dt.Rows[0][12].ToString();
-                companyBox.Text = dt.Rows[0][14].ToString();
-                accBox.Text = dt.Rows[0][15].ToString();
+                //        bdayBox.Value = date1;
+                packageBox.SelectedItem = dt.Rows[0][11].ToString();
+                paymentStatusBox.SelectedItem = dt.Rows[0][9].ToString();
+                companyBox.Text = dt.Rows[0][12].ToString();
+                accBox.Text = dt.Rows[0][13].ToString();
 
 
                 clearAll.Enabled = true;
